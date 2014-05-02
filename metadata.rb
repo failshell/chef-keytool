a8e14b5d6d772799b5207306617f1bb42e8f1ed8
Jean-Francois Theroux <jtheroux@lapresse.ca>
2014-05-02 17:06:32 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'me@failshell.io'
 license          'Apache 2.0'
 description      'Installs/Configures keytool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
