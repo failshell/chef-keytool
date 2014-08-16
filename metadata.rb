31c2972289452cdd2538c458f79d941c193e528f
Jean-Francois Theroux <me@failshell.io>
2014-08-15 23:11:28 -0400

    added supported OS list to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,13 @@ license          'Apache 2.0'
 description      'Installs/Configures keytool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.4'
+
+%w(
+  redhat
+  centos
+  debian
+  ubuntu
+  suse
+).each do |os|
+  supports os
+end
